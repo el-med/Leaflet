23e214593bd97acd5d6ec42a6cfc5a3af705af47
m.elharrak <med.elharrak@gmail.com>
2014-03-04 15:54:07 +0000

    Basic wrapper for Leaflet

---
+++ designer.js
@@ -0,0 +1,41 @@
+(function(LeafletjsMap) {
+
+//    /* Default width and height of your widget */
+    LeafletjsMap.setWidth('600');
+//    LeafletjsMap.setHeight('20');
+
+//    /* Define custom event for your widget */
+//    LeafletjsMap.addEvent('myEvent');
+
+//    /* Customize existing properties */
+//    LeafletjsMap.customizeProperty('test', {
+//        sourceTitle: 'Test Source',
+//        title: 'Test Static Value',
+//        description: 'Add a datasource to this property.'
+//    });
+
+//    /* Add a Label property */
+//    LeafletjsMap.addLabel({
+//        'defaultValue': '',
+//        'position': 'top'
+//    });
+
+//    /* Set the Design and Styles panels */
+//    LeafletjsMap.setPanelStyle({
+//        'fClass': true,
+//        'text': true,
+//        'background': true,
+//        'border': true,
+//        'sizePosition': true,
+//        'label': true,
+//        'disabled': ['border-radius']
+//    });
+
+//    /* Override widget's initialization */
+//    LeafletjsMap.prototype.init = function() {
+//        this.node.innerHTML = "Widget Text"; /* Include text inside the widget */
+//    }
+
+});
+
+// For more information, refer to http://doc.wakanda.org/Wakanda0.DevBranch/help/Title/en/page3870.html
---
+++ widget.js
@@ -0,0 +1,49 @@
+/*global WAF, $, L */
+
+'use strict';
+
+WAF.define('LeafletjsMap', ['waf-core/widget'], function (widget) {
+
+    var LeafletjsMap = widget.create('LeafletjsMap', {
+
+        items: widget.property({
+            type: 'datasource',
+            attributes: [{
+                name: 'lat'
+            }, {
+                name: 'lan'
+            }]
+        }),
+
+        init: function () {
+            var self = this,
+                map = L.map(this.node),
+                osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                osmAttrib = 'Map data &copy; OpenStreetMap contributors',
+                osm = new L.TileLayer(osmUrl, {
+                    attribution: osmAttrib
+                });
+
+            map.addLayer(osm);
+
+
+            if (this.items()) {
+                this.items().addListener('currentElementChange', function () {
+                    var marker,
+                        items = self.items();
+
+                    if (items.lat) {
+                        marker = L.marker([items.lan, items.lat]).addTo(map);
+                        marker.bindPopup('a marker for ' + items.name.toUpperCase());
+                        map.setView(new L.LatLng(items.lan, items.lat), 9);
+                    }
+                });
+            }
+        }
+    });
+
+    return LeafletjsMap;
+
+});
+
+/* For more information, refer to http://doc.wakanda.org/Wakanda0.DevBranch/help/Title/en/page3871.html */
